ef71e53f6bf7ac095400f99cd1cab80d4c071980
Girish Sharma <girish.sharma1020@gmail.com>
2019-11-24 00:30:59 +0530

    class for network check. changed compile sdk to 28

---
+++ app/src/main/java/com/example/androidassignment/utils/CheckInternet.kt
@@ -0,0 +1,19 @@
+package com.example.androidassignment.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+
+/**
+ * @author girishsharma
+ * Class for checking network connectivity
+ * */
+class CheckInternet {
+
+    companion object {
+        fun networkOn(context: Context): Boolean {
+            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val activeNetwork = cm.activeNetworkInfo
+            return activeNetwork != null && activeNetwork.isConnected
+        }
+    }
+}

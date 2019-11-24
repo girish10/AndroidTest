eabcfa31a361843c3044d10b4f68ca17fc465e3a
Girish Sharma <girish.sharma1020@gmail.com>
2019-11-24 13:05:25 +0530

    Not internet connectivity handling.

--- app/src/main/java/com/example/androidassignment/MainActivity.kt
+++ app/src/main/java/com/example/androidassignment/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.androidassignment
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import butterknife.ButterKnife
 import com.example.androidassignment.adapter.FeedsAdapter
@@ -21,17 +24,23 @@ import java.time.Duration
  * Main class for calling the api and displaying the data on the UI
  * */
 
-class MainActivity : FragmentActivity() , SwipeRefreshLayout.OnRefreshListener {
+class MainActivity : FragmentActivity(), SwipeRefreshLayout.OnRefreshListener {
 
     lateinit var feedVm: feedViewModel
 
     override fun onRefresh() {
         if (CheckInternet.networkOn(this)) {
+            toggleVisibility(recylerViewMain, txtViewMainScreen)
             swipeRefreshMain.isRefreshing = true
             fetchFeed()
-        }else{
+        } else {
             swipeRefreshMain.isRefreshing = false
-            Toast.makeText(this,resources.getString(R.string.textNetwork).toString(),Toast.LENGTH_LONG).show()
+            toggleVisibility(txtViewMainScreen, recylerViewMain)
+            Toast.makeText(
+                this,
+                resources.getString(R.string.textNetwork).toString(),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
@@ -52,19 +61,34 @@ class MainActivity : FragmentActivity() , SwipeRefreshLayout.OnRefreshListener {
         val factory = feedViewModel.Factory(this.application);
         feedVm = ViewModelProviders.of(this, factory).get(feedViewModel::class.java)
         feedVm.fetchFeed()
-        feedVm.feedObserver!!.observe(this, Observer {
-            titleTextMain.setText(it.title)
-            loadFeedRows(it.arrayFeedRows)
+        feedVm.feedObserver!!.observe(this, Observer { jsonFeed ->
+            if (jsonFeed != null) {
+                titleTextMain.text = jsonFeed.title
+                loadFeedRows(jsonFeed.arrayFeedRows)
+            }
             swipeRefreshMain.isRefreshing = false
         })
     }
 
     private fun loadFeedRows(arrayFeedRows: List<jsonFeedRow>) {
-        val rowsAdapter = FeedsAdapter(arrayFeedRows, this)
-        val decoration = DividerItemDecoration(this,
-            DividerItemDecoration.VERTICAL)
-        recylerViewMain.layoutManager = LinearLayoutManager(this)
-        recylerViewMain.addItemDecoration(decoration)
-        recylerViewMain.adapter = rowsAdapter
+        if (arrayFeedRows.isNotEmpty()) {
+            toggleVisibility(recylerViewMain, txtViewMainScreen)
+            val rowsAdapter = FeedsAdapter(arrayFeedRows, this)
+            val decoration = DividerItemDecoration(
+                this,
+                DividerItemDecoration.VERTICAL
+            )
+            recylerViewMain.layoutManager = LinearLayoutManager(this)
+            recylerViewMain.addItemDecoration(decoration)
+            recylerViewMain.adapter = rowsAdapter
+        } else {
+            toggleVisibility(txtViewMainScreen, recylerViewMain)
+            txtViewMainScreen.text = resources.getString(R.string.textNoData)
+        }
+    }
+
+    private fun toggleVisibility(view: View, hideView: View) {
+        hideView.visibility = View.GONE
+        view.visibility = View.VISIBLE
     }
 }
